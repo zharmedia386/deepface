82906d05892abe07f6332b4c61f8d51c3ab09311
M Azhar Alauddin <80077273+zharmedia386@users.noreply.github.com>
2022-12-18 17:57:53 +0700

    Represent, Analyze Deepface

--- api/static/index.js
+++ api/static/index.js
@@ -1,83 +1,55 @@
-function getText() {
-	$('#my-form').on('submit', function (event) {
-		event.preventDefault();
-		let text1 = $(this).find('#text1').val();
+const input = document.getElementById("selectAvatar");
+const avatar = document.getElementById("avatar");
+const textArea = document.getElementById("textArea");
+let reqBase64 = {};
 
-		$.ajax({
-			url: "/analyze",
-			type: 'post',
-			dataType: 'json',
-			async: false,
-			contentType: "application/json; charset=utf-8",
-			data: text1,
-			success: function(response){
-				console.log("success loh")
-				console.log(response);
-				$("textarea[id='text2']").css("display", "block");
-				response = JSON.stringify(response, null, "\t");
-				$("#text2").val(response);
-			},
-			error: function(data){
-				console.log("error")
-				console.log(data);
-				// console.log(error);
-			}
-		});
+const convertBase64 = (file) => {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+
+    fileReader.onload = () => {
+      resolve(fileReader.result);
+    };
+
+    fileReader.onerror = (error) => {
+      reject(error);
+    };
+  });
+};
+
+const uploadImage = async (event) => {
+  const file = event.target.files[0];
+  const base64 = await convertBase64(file);
+  avatar.src = base64;
+  textArea.innerText = base64;
+  reqBase64 = { img: [base64] };
+  reqBase64 = JSON.stringify(reqBase64);
+};
+
+
+// ANALYZE - DEEPFACE
+input.addEventListener("change", (e) => {
+	uploadImage(e);
+		console.log(reqBase64)
+	$.ajax({
+		url: "/analyze",
+		type: 'post',
+		dataType: 'json',
+		async: false,
+		contentType: "application/json; charset=utf-8",
+		data: reqBase64,
+		success: function(response){
+			console.log("success loh")
+			console.log(response);
+			$("textarea[id='text2']").css("display", "block");
+			response = JSON.stringify(response, null, "\t");
+			$("#text2").val(response);
+		},
+		error: function(data){
+			console.log("error")
+			console.log(data);
+			// console.log(error);
+		}
 	});
-}
-
-function getTextVerify() {
-	$('#my-form').on('submit', function (event) {
-		event.preventDefault();
-		let text1 = $(this).find('#text1').val();
-
-		$.ajax({
-			url: "/verify",
-			type: 'post',
-			dataType: 'json',
-			async: false,
-			contentType: "application/json; charset=utf-8",
-			data: text1,
-			success: function(response){
-				console.log("success loh")
-				console.log(response);
-				$("textarea[id='text2']").css("display", "block");
-				response = JSON.stringify(response, null, "\t");
-				$("#text2").val(response);
-			},
-			error: function(data){
-				console.log("error")
-				console.log(data);
-				// console.log(error);
-			}
-		});
-	});
-}
-
-function getTextRepresent() {
-	$('#my-form').on('submit', function (event) {
-		event.preventDefault();
-		let text1 = $(this).find('#text1').val();
-
-		$.ajax({
-			url: "/represent",
-			type: 'post',
-			dataType: 'json',
-			async: false,
-			contentType: "application/json; charset=utf-8",
-			data: text1,
-			success: function(response){
-				console.log("success loh")
-				console.log(response);
-				$("textarea[id='text2']").css("display", "block");
-				response = JSON.stringify(response, null, "\t");
-				$("#text2").val(response);
-			},
-			error: function(data){
-				console.log("error")
-				console.log(data);
-				// console.log(error);
-			}
-		});
-	});
-}
+});
